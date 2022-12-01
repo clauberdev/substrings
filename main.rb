0eec155eb832354fbf3d9a1cb4bee53f820c070d
Clauber <55664564+clauberdev@users.noreply.github.com>
2022-12-01 00:48:10 +0000

    Desafio concluído.

--- main.rb
+++ main.rb
@@ -1,11 +1,17 @@
-puts "Hello, World!"
-
-def substrings(word, arr)
-   arr
+def substrings(str, dict)
+    listsubstr = Hash.new(0)
+    arr = str.downcase.split()
+    for i in dict
+        for j in arr 
+            if j.include?(i) == true 
+                listsubstr[i] += 1
+                next
+            end
+        end
+    end
+    return listsubstr
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-p substrings("below", dictionary)
-
-p substrings("Howdy partner, sit down! How's it going?", dictionary) # { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+p substrings('below', dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
